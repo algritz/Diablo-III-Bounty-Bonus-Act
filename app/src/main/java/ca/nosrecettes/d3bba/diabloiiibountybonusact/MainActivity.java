85185c6fec59d2543aef2e172935bb989872a432
David Cloutier <david.cloutier@oracle.com>
2016-05-27 20:45:32 -0400

    fixed bugs for lists calculations

--- app/src/main/java/ca/nosrecettes/d3bba/diabloiiibountybonusact/MainActivity.java
+++ app/src/main/java/ca/nosrecettes/d3bba/diabloiiibountybonusact/MainActivity.java
@@ -48,31 +48,29 @@ public class MainActivity extends AppCompatActivity {
 
         remaining_cycle_index = (current_hour + offset + 2) % 20;
 
-        /* if (remaining_cycle_index == 20) {
-            remaining_cycle_index = 0;
-        }
+        SimpleDateFormat starting_format = new SimpleDateFormat(" HH.00:00");
+        SimpleDateFormat ending_format = new SimpleDateFormat(" HH.59:59");
 
-        if (next_cycle_index == 20) {
-            next_cycle_index = 0;
-            remaining_cycle_index = 1;
-        }
-        */
-        SimpleDateFormat starting_format = new SimpleDateFormat(" H.00:00");
-        SimpleDateFormat ending_format = new SimpleDateFormat(" H.59:59");
+        SimpleDateFormat full_starting_format = new SimpleDateFormat("MMM/dd/yy HH.00:00");
 
-        SimpleDateFormat full_starting_format = new SimpleDateFormat("MMM/dd/yy H.00:00");
-
-        current_cycle = "Current Cycle | until " + ending_format.format(timeStamp) + " - " + bountyArray[(int) current_cycle_index];
-        next_cycle = "Next Cycle | Starting " + starting_format.format((timeStamp + 3600000)) + " - " + bountyArray[(int) next_cycle_index];
+        current_cycle = "Current Cycle | until       " + ending_format.format(timeStamp) + " - " + bountyArray[(int) current_cycle_index];
+        next_cycle = "Next Cycle      | Starting " + starting_format.format((timeStamp + 3600000)) + " - " + bountyArray[(int) next_cycle_index];
 
         currentCycleTextView.setText(current_cycle);
         nextCycleTextView.setText(next_cycle);
 
-        for (int i = current_cycle_index+2; i < 2; ++i) {
-            remaining_cycle.add("Cycle: " + current_cycle_index + " ## " + full_starting_format.format((timeStamp + (3600000 * (i)))) + " - " + bountyArray[i]);
+        for (int i = current_cycle_index, j = current_cycle_index + 3, k = current_cycle_index + 2; i < 19; ++i, j++, k++) {
+            if (k == 20) {
+                k = 0;
+            }
+            if (j == 21) {
+                j = 1;
+            }
+            remaining_cycle.add("Cycle # " + String.format("%02d", j) + " - " + full_starting_format.format((timeStamp + (3600000 * (i)))) + " - " + bountyArray[k]);
         }
-        for (int i = 0; i <= current_cycle_index - 1; i++) {
-            remaining_cycle.add("Cycle: " + i + " -- " + full_starting_format.format((timeStamp + (3600000 * (i + (20 - current_cycle_index))))) + " - " + bountyArray[i]);
+
+        for (int i = 0, j = current_cycle_index, k = current_cycle_index - 3, l = current_cycle_index - 1; i <= current_cycle_index; i++, j++, k++, l++) {
+            remaining_cycle.add("Cycle # " + String.format("%02d", j) + " -- " + full_starting_format.format((timeStamp + (3600000 * (k + 20)))) + " - " + bountyArray[l]);
         }
 
         bountyListView = (ListView) findViewById(R.id.bounty_list);
